dd50fde1363cb690717484f1484ad10ca8606001
bytesandcookiesindore <bytesandcookiesindore@gmail.com>
2026-05-07 19:34:18 +0530

    refactor: extract for salary calculation logic

--- src/controllers/salary.controller.js
+++ src/controllers/salary.controller.js
@@ -2,7 +2,8 @@ const salaryService = require("../services/salary.service");
 exports.getSalary = (req, res, next) => {
   try {
     const gross = Number(req.query.gross);
-    const result = salaryService.getSalaryByEmployeeId(req.params.id, gross);
+    const { id } = req.params;
+    const result = salaryService.getSalaryByEmployeeId(id, gross);
     res.status(200).json(result);
   } catch (err) {
     next(err);
